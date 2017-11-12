3b63b936294193f85ccac7d1d2998fe9f3c211fc
sunny <haguluma0@yahoo.co.jp>
2017-11-12 11:27:03 +0900

    ソースコードを軽く整理

--- server.rb
+++ server.rb
@@ -5,7 +5,6 @@ require 'json'
 require 'sinatra'
 require 'sinatra/reloader'
 
-
 #データベース関連
 require 'sqlite3'
 require 'active_record'
@@ -15,12 +14,9 @@ ActiveRecord::Base.establish_connection(
 )
 class Test < ActiveRecord::Base
 end
-# ・・・こんな感じで読み込む　テーブル名は最後にsをつけること ex members
 
-#'/'はそのページのルートを表す
-get '/' do
-  erb :index
-#erb :テンプレート名　はそのテンプレートを返す
+get '/' do  #'/'はそのページのルートを表す
+  erb :index  #erb :テンプレート名　はそのテンプレートを返す
 end
 
 post'/database' do
@@ -30,7 +26,7 @@ post'/database' do
     file_id = params[:file_id]
     file_text = params[:file_text]
     Test.create(:file_id=>file_id,:file_text=>file_text)
-    File.open('./public/testfiles/'+file_id+'.txt','w+:utf-8')do|file|
+    File.open('./public/testfiles/'+file_id+'.txt','w+:utf-8') do |file|
       file.write(file_text+"\n")
       file.close
     end
